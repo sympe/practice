f140a0ecede47f934c88ee1dfe81a14a9ccc019e
辻慎平 <1st.link.sukimax@gmail.com>
2017-11-04 15:23:43 +0900

    atcoder練習問題

---
+++ at_coder/practice_contest/a.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+  ios::sync_with_stdio(false);
+  int a;
+  int b;
+  int c;
+  string s;
+  cin >> a >> b >> c >> s;
+  cout << (a + b + c) << " " << s << "\n";
+
+  return 0;
+}
