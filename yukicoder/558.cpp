7e0ea0ff623bf75c795d57d2c9230478e7b0d9ca
辻慎平 <1st.link.sukimax@gmail.com>
2017-11-28 23:42:59 +0900

    11/28

--- yukicoder/558.cpp
+++ yukicoder/558.cpp
@@ -4,15 +4,17 @@ using namespace std;
 int main ()
 {
   ios::sync_with_stdio(false);
+  int n;
   string str;
+  string ans = '';
 
   cin >> str;
-
-  if (str.find("575") > 0) {
-    cout << "YES" << "\n";
-  } else {
-    cout << "NO" << "\n";
+  for (int i = 0; i < n; i++) {
+    cin >> str;
+    ans = ans + str;
   }
+
+  cout << ans << "\n";
 
   return 0;
 }
