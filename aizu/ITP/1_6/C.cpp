8715ca5b256eda2715f785c95e91226287fcddd2
辻慎平 <1st.link.sukimax@gmail.com>
2017-12-06 23:42:45 +0900

    12/6

--- aizu/ITP/1_6/C.cpp
+++ aizu/ITP/1_6/C.cpp
@@ -13,21 +13,61 @@ int main ()
   int fourth[3][10];
   int b, f, r, v;
 
+  for (int i = 0; i < 3; i++) {
+    for (int j = 0; j < 10; j++) {
+      first[i][j] = 0;
+      second[i][j] = 0;
+      third[i][j] = 0;
+      fourth[i][j] = 0;
+    }
+  }
+
+
   scanf("%d", &n);
 
   for (int i = 1; i <= n; i++) {
     scanf("%d %d %d %d", &b, &f, &r, &v);
     if (b == 1){
-      first[f - 1][r - 1] = v;
+      first[f - 1][r - 1] += v;
     } else if (b == 2) {
-      second[f - 1][r - 1] = v;
+      second[f - 1][r - 1] += v;
     } else if (b == 3) {
-      third[f - 1][r - 1] = v;
+      third[f - 1][r - 1] += v;
     } else if (b == 4) {
-      fourth[f - 1][r - 1] = v;
+      fourth[f - 1][r - 1] += v;
     }
   }
-  printf("\n");
+
+  for (int i = 0; i < 3; i++) {
+    for (int j = 0; j < 10; j++) {
+      printf(" %d", first[i][j]);
+    }
+    printf("\n");
+  }
+  printf("####################\n");
+
+  for (int i = 0; i < 3; i++) {
+    for (int j = 0; j < 10; j++) {
+      printf(" %d", second[i][j]);
+    }
+    printf("\n");
+  }
+  printf("####################\n");
+
+  for (int i = 0; i < 3; i++) {
+    for (int j = 0; j < 10; j++) {
+      printf(" %d", third[i][j]);
+    }
+    printf("\n");
+  }
+  printf("####################\n");
+
+  for (int i = 0; i < 3; i++) {
+    for (int j = 0; j < 10; j++) {
+      printf(" %d", fourth[i][j]);
+    }
+    printf("\n");
+  }
 
   return 0;
 }
