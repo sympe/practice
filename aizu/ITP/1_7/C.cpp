b68dc8d971861ea638c661a8c862928f6c2f4346
辻慎平 <1st.link.sukimax@gmail.com>
2017-12-18 23:39:32 +0900

    12/18

--- aizu/ITP/1_7/C.cpp
+++ aizu/ITP/1_7/C.cpp
@@ -8,7 +8,9 @@ int main ()
 {
   int r, c;
   int A[100][100];
+  int B[100];
   int x;
+  int sum = 0;
   scanf("%d %d", &r, &c);
 
   for (int i = 0; i < r; i++) {
@@ -17,12 +19,25 @@ int main ()
     }
   }
 
+  for (int j = 0; j < c; j++) {
+    x = 0;
+    for (int i = 0; i < r; i++) {
+      x += A[i][j];
+    }
+    B[j] = x;
+    sum += x;
+  }
+
   for (int i = 0; i <= r; i++) {
     x = 0;
     for (int j = 0; j <= c; j++) {
       x += A[i][j];
       if (i == r) {
-        printf("%d ", 1);
+        if (j == c) {
+          printf("%d\n", sum);
+        } else {
+          printf("%d ", B[j]);
+        }
       } else {
         if (j == c) {
           printf("%d\n", x);
